1a64fb1b5747c1065ba8c7356ddfacc61b59327e
eddycolloton <eddy.colloton@gmail.com>
2023-11-06 08:59:31 -0700

    fixed bug, 'current_section' variable and original readline for loop was not working, new for loop is more precise'

--- mediainfo_check.py
+++ mediainfo_check.py
@@ -5,23 +5,23 @@ def parse_mediainfo(file_path):
     expected_general = {
         "File extension": "mkv",
         "Format": "Matroska",
-        "Overall bit rate mode": "VBR",
+        "Overall bit rate mode": "Variable",
     }
     # creates a dictionary of expected keys and values for the mediainfo output section "Video"
     expected_video = {
         "Format": "FFV1",
         "Format settings, GOP": "N=1",
         "Codec ID": "V_MS/VFW/FOURCC / FFV1",
-        "Width": "720",
-        "Height": "486",
+        "Width": "720 pixels",
+        "Height": "486 pixels",
         "Pixel aspect ratio": "0.900",
-        "Display aspect ratio": "1.333",
-        "Frame rate mode": "CFR",
-        "Frame rate": "29.970",
+        "Display aspect ratio": "4:3",
+        "Frame rate mode": "Constant",
+        "Frame rate": "29.970 (30000/1001) FPS",
         "Standard": "NTSC",
         "Color space": "YUV",
         "Chroma subsampling": "4:2:2",
-        "Bit depth": "10",
+        "Bit depth": "10 bits",
         "Scan type": "Interlaced",
         "Scan order": "Bottom Field First",
         "Compression mode": "Lossless",
@@ -37,65 +37,77 @@ def parse_mediainfo(file_path):
     expected_audio = {
         "Format": "FLAC",
         "Channel(s)": "2 channels",
-        "Channel positions": "Front: L R",
-        "Sampling rate": "48000",
-        "Bit depth": "24",
+        "Sampling rate": "48.0 kHz",
+        "Bit depth": "24 bits",
         "Compression mode": "Lossless",
         "Writing library": "Lavc59.37.100 flac",
     }
 
-    with open(file_path, 'r') as file:
-    # open mediainfo text file as variable "file"
-        lines = file.readlines()
-        # define variable 'lines' as all individual lines in file (to be parsed in next "for loop")
+    # creates a list of fields to check, but only to check that the fiels is not empty
+    expected_custom_fields = {
+        "Title": "",
+        "Encoded by": "",
+        "Description": "",
+        "Encoding settings": "",
+        "ORIGINAL MEDIA TYPE": "",
+    }
 
-    current_section = None
-    # creates empty variable "current_section"
     section_data = {}
     # creates empty dictionary "section_data"
-    # this dictionary will actually store 3 separate dictionaries inside of it, one for each section
+    # this dictionary will actually store 3 separate dictionaries inside of it (called a "nested dictionary"), one for each section
+    section_data["General"] = {}
+    # nested dicitonary for storing data from "General" section
+    section_data["Video"] = {}
+    # nested dicitonary for storing data from "Video" section
+    section_data["Audio"] = {}
+    # nested dicitonary for storing data from "Audio" section
 
     ## Explination of for loop below:
-    # The 'for loop' below goes through the mediainfo text file line by line, one at a time
-    # if the line matches one of the 3 sections, it assigns the section to the variable "current_section" 
-    # and creates a new dictionary within the "section_data" dictionary named after the section: General, Video or Audio
-    # if the line does not match one of the 3 sections, it is a line with a mediainfo field and corresponding value
-    # a line that does not match one of the 3 sections will be below one fo the 3 section headers, and therefore the "current_section" variable will be assigned
     # the mediainfo field and value are then assigned to the current section's dictionary, which is stored within the section_data dictionary
     
-    for line in lines:
-    # for each line in mediainfo text file
-        line = line.strip()
-        # strips line of blank space with python function strip()
-        if line.startswith("General"):
-            current_section = "General"
-            # if line starts with "General", assigns variable current_section to "General"
-            section_data[current_section] = {}
-            # creates new "General" dictionary within "section_data" dictionary
-        elif line.startswith("Video"):
-            current_section = "Video"
-            # if line starts with "Video", assigns variable current_section to "Video"
-            section_data[current_section] = {}
-            # creates new "Video" dictionary within "section_data" dictionary
-        elif line.startswith("Audio"):
-            current_section = "Audio"
-            # if line starts with "Audio", assigns variable current_section to "Audio"
-            section_data[current_section] = {}
-            # creates new "Audio" dictionary within "section_data" dictionary
-        elif line:
-        # if line does not start with General, Video, or Audio, then:
-            key, value = [x.strip() for x in line.split(":", 1)]
-            #assign variable "key" to string before ":" and variable "value" to string after ":"
-            if current_section and key in section_data[current_section]:
-                # if the variable "current_section" is assigned and matches one of the new dictionaries
-                # and if the variable "key" is assigned, then:
-                section_data[current_section][key] = value
-                # key is a key in the new dictionary (General, Video, or Audio) and value is matched to the key, in a key:value pair
+    with open(file_path, 'r') as file:
+    # open mediainfo text file as variable "file"
+        for line in iter(lambda: file.readline().rstrip(), 'Video'):
+        # for each line in mediainfo text file, read the line, strip out any blank space, and stop when you reach 'Video'
+        # I found this technique here: https://stackoverflow.com/questions/27135499/read-file-until-specific-line-in-python
+            if line.startswith("General"):
+            # if the line starts with "General"
+                continue
+                # do nothing
+            elif line:
+            # for every other line (until 'Video'), do:
+                key, value = [x.strip() for x in line.split(":", 1)]
+                # assign variable "key" to string before ":" and variable "value" to string after ":"
+                section_data["General"][key] = value
+                # add key: value pair to nested dictionary 
+        for line in file:
+        # For the next lines in the file, do this:
+        # (This "for" loop will pick up where the previous one ended, the line right after 'Video')
+            if ":" in line:
+            # if ":" is in the line (basically, if the line is not 'Audio')
+                key, value = [x.strip() for x in line.split(":", 1)]
+                # assign variable "key" to string before ":" and variable "value" to string after ":"
+                section_data["Video"][key] = value
+                # add key: value pair to nested dictionary 
+            elif line.startswith("Audio"):
+                break
+                # if line starts with "Audio" stop
+                # Not sure why, but the 'lambda' technique was causing a key error for this section, either on the blank line before 'Audio' or on 'Audio'.
+                # After a lot of troubleshooting, just decided to do it this way instead
+        for line in file:
+        # For the next lines in the file, do this:
+        # (This "for" loop will pick up where the previous one ended, the line right after 'Audio')
+        # https://stackoverflow.com/questions/27805919/how-to-only-read-lines-in-a-text-file-after-a-certain-string
+            if ":" in line:
+                key, value = [x.strip() for x in line.split(":", 1)]
+                # assign variable "key" to string before ":" and variable "value" to string after ":"
+                section_data["Audio"][key] = value
+                # add key: value pair to nested dictionary 
 
     ## Explination of the loops below:
     # The loops below assign the variables "expected_key" and "expected_value" to the key:value pairs in the "expected" dictionaries defined at the beginning of the function
-    # the variable "actual_value" is used to define the value to the key matching the "expected_yey" in the section_data[current_section] dictionary (defined in the loop above)
-    # if the actual_value variable and the expected_valu variable don't match, then a string stating both values is appened to a list called "differences"
+    # the variable "actual_value" is used to define the value to the key that matches 'expected_key' in the section_data nested dictionaries (defined in the loop above)
+    # if the actual_value variable and the expected_value variable don't match, then a string stating both values is appened to a list called "differences"
 
     differences = []
     # Create empty list, "differences"
@@ -134,6 +146,13 @@ def parse_mediainfo(file_path):
                 differences.append(f"Audio: {expected_key}\nExpected: {expected_value}\nActual: {actual_value}\n")
                 # append this string to the list "differences"
 
+    for expected_key, expected_value in expected_custom_fields.items():
+    # defines variables "expected_key" and "expected_value" to the dictionary "expected_audio"
+        if len(section_data["General"][expected_key]) == 0:
+        # count the values in the nested dictionary "General" with 'len', if the values are zero, then:
+            differences.append(f"General: {expected_key} is empty")
+            # append this string to the list "differences"
+    
     if not differences:
     # if the list "differences" is empty, then
         print("All specified fields and values found in the MediaInfo output.")
@@ -143,7 +162,7 @@ def parse_mediainfo(file_path):
         for diff in differences:
             print(diff)
 
-file_path = "JPCspecs_mi.txt"
+file_path = "empty-field_JPCspecs_mi.txt"
 # assigns variable "file_path" to the text file "JPCspecs_mi.txt"
 # This part of the script is for testing purposes and it will need to change to assign file_path programatically when run on a directory or something... TBD
 
