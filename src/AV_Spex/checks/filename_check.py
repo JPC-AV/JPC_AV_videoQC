ba0fe830b2eaf40c2d78435f946eadb38846bfa4
eddycollotonn <eddy.colloton@gmail.com>
2024-09-23 15:15:05 -0600

    added exception to file name check tow owrk with bowser files

--- src/AV_Spex/checks/filename_check.py
+++ src/AV_Spex/checks/filename_check.py
@@ -8,9 +8,8 @@ from ..utils.find_config import config_path, command_config
 
 def is_valid_filename(video_filename):
     '''
-    Locates approved values for the file name, stored in key:value pairs under 'filename_values' in config/config.yaml
-    The file name pattern is in 5 sections: Collection, ObjectID, Media type, Digital generation, and file extension
-    Approved values for each of these sections are stored in config/config.yaml
+    Locates approved values for the file name, stored in key:value pairs under 'filename_values' in config/config.yaml.
+    The file name pattern varies and is constructed dynamically depending on the provided config.
     '''
     valid_filename = False
     
@@ -20,14 +19,22 @@ def is_valid_filename(video_filename):
     # Get only the base filename (not the full path)
     base_filename = os.path.basename(video_filename)
 
-    # Then piece the parts of the approved values into the file name pattern that the input video will be tested against
-    # The pattern has to account for the last value in the approved_values dict being a file extension instead of a file name "field"
-    pattern = r'^{0}_{1}_{2}\.{3}$'.format(
+    # Build the dynamic regex pattern based on the keys in the config
+    pattern_parts = [
         re.escape(approved_values['Collection']),
         re.escape(approved_values['MediaType']),
-        approved_values['ObjectID'],  # ObjectID is already a regex pattern, so no escaping here
-        re.escape(approved_values['FileExtension'])
-    )
+        approved_values['ObjectID']  # ObjectID can contain a regex, so no escaping
+    ]
+    
+    # Check if 'DigitalGeneration' is part of the convention and include it in the pattern if present
+    if 'DigitalGeneration' in approved_values:
+        pattern_parts.append(re.escape(approved_values['DigitalGeneration']))
+    
+    # Append the file extension
+    file_extension = re.escape(approved_values['FileExtension'])
+    
+    # Construct the complete pattern, joining the parts and accounting for the file extension
+    pattern = r'^{0}\.{1}$'.format('_'.join(pattern_parts), file_extension)
     
     # Check if the filename matches the pattern
     if re.match(pattern, base_filename, re.IGNORECASE):
