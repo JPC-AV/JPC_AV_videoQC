aedf4a75bc913cc7f5db51391988fd9b1df05668
eddycollotonn <eddy.colloton@gmail.com>
2025-01-29 21:11:53 -0700

    pyinstaller can build gui (w/out logos) using command: pyinstaller --clean avspex.spec

--- src/AV_Spex/av_spex_the_file.py
+++ src/AV_Spex/av_spex_the_file.py
@@ -88,7 +88,8 @@ def parse_arguments():
     # Get the version from pyproject.toml
     if getattr(sys, 'frozen', False):
         # If running as bundled executable
-        project_path = os.path.dirname(os.path.dirname(sys._MEIPASS))
+        base_path = sys._MEIPASS
+        project_path = base_path  # Set project_path for frozen app
     else:
         # If running from source
         project_path = os.path.dirname(os.path.dirname(config_mgr._bundle_dir))
